f1d2b818ea6b2e9a658a51ecc18913819dd7ac71
KareemKallash <kareemkallash18@gmail.com>
2024-07-31 17:43:36 +0300

    Delete BackEnd/server.js

--- BackEnd/server.js
+++
@@ -1,8 +0,0 @@
-//we create server here 
-const http = require('http');
-const app=require('./app');
-const port = process.env.port ||3000;
-const server = http.createServer(app);
-
-server.listen(port);//to start the server
-
