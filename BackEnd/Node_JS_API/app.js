d7e7c8eb0fb6d32a77b402bd52d32ae502e69dfc
KareemKallash <kareemkallash18@gmail.com>
2024-08-13 14:44:31 +0300

    Update app.js

--- BackEnd/Node_JS_API/app.js
+++ BackEnd/Node_JS_API/app.js
@@ -9,6 +9,9 @@ const axios = require('axios');
 
 const predictionRoutes = require('./api/routes/Prediction');
 const userRoutes= require('./api/routes/users');
+const scraperRoutes = require('./api/routes/scrap'); 
+app.use('/scraper', scraperRoutes);
+
 const mongoURI = 'mongodb://localhost:27017/';  
 
 mongoose.connect(mongoURI);
@@ -26,20 +29,11 @@ app.use((req, res, next) => {
     next();//حتى لا اعمل بلوك للركوست تبعي
 });
 
-app.post('/predict', async (req, res, next) => {
-    try {
-        const response = await axios.post('http://0.0.0.0:3000/predict', req.body);
-        res.status(200).json(response.data);
-    } catch (error) {
-        next(error);
-    }
-});
-
-
 // //Routes which should handle requests 
-
 app.use('/Prediction',predictionRoutes)
 app.use('/users', userRoutes);
+app.use('/scraper', scraperRoutes);
+
 
 app.use((req, res, next) => {
     const error = new Error('Not found');//في حال ما لقى بيلي فوق بنزل لهون 
@@ -57,5 +51,6 @@ app.use((error, req, res, next) => {//handle all errors
     )
 })
 module.exports = app;
+
 
 
