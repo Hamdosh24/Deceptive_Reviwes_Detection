1c1f70cae9b4484225d79cfb1728a5c7f5f4c018
KareemKallash <kareemkallash18@gmail.com>
2024-08-15 16:21:43 +0300

    Update api.py

--- BackEnd/Model_API_Flask/api.py
+++ BackEnd/Model_API_Flask/api.py
@@ -1,26 +1,25 @@
+
 from flask import Flask, request, jsonify
 from model_utils import get_prediction
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def prediction():
+    print("Received a request on /predict")  # طباعة رسالة عند تلقي الطلب
     if request.method == 'POST':
-        input_texts = request.json
+        input_texts = request.json.get('inputTexts', [])
         out_put = []
 
         for text in input_texts:
-            pred_label = get_prediction(text['text'])
+            pred_label = get_prediction(text)  # الحصول على التصنيف
             out_put.append({
-                "id": text["id"],
-                "label": pred_label
+                "text": text,  # النص المدخل
+                "label": pred_label  # التصنيف
             })
+        print(f"Output: {out_put}")  # طباعة الناتج
         return jsonify(out_put)
-    else:
-        raise TypeError(f'The request type must by POST, get {request.method}')
-
-
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000,debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
