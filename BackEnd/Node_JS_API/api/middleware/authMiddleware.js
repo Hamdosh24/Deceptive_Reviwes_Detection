aae5b37a628c324f3c837de2f1c89934287f5b90
KareemKallash <kareemkallash18@gmail.com>
2024-08-19 20:09:02 +0300

    Add files via upload

---
+++ BackEnd/Node_JS_API/api/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({
+                message: 'No token provided'
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = decoded;
+        next(); // متابعة إلى الميدل وير التالي
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Invalid token'
+        });
+    }
+};
