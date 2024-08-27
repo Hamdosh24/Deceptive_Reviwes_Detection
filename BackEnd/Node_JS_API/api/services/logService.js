d3b143bd6787ebc081df59f49ff8aca47ab17eae
KareemKallash <kareemkallash18@gmail.com>
2024-08-27 12:31:37 +0300

    Add files via upload

--- BackEnd/Node_JS_API/api/services/logService.js
+++ BackEnd/Node_JS_API/api/services/logService.js
@@ -1,21 +1,47 @@
+const AdminUsageLog = require('../models/AdminUsageLog'); 
 const UsageLog = require('../models/UsageLog'); 
 
-async function logUsage(userId, serviceUsed, { url, text, label }) {
+async function logUsageData(LogModel, userId, serviceUsed, data) {
+    if (!userId || !serviceUsed) {
+        throw new Error('Required parameters (userId, serviceUsed) are missing');
+    }
+
     try {
-        const newLog = new UsageLog({
-            userId,
-            serviceUsed,
-            url: url || null,  
-            text,  
-            label,
-            createdAt: new Date()
-        });
-        await newLog.save(); 
+        if (Array.isArray(data)) {
+            const logs = data.map(item => ({
+                userId,
+                serviceUsed,
+                url: null,
+                text: item.text || 'No text',
+                label: item.label || 'No label',
+                createdAt: new Date()
+            }));
+            await LogModel.insertMany(logs); 
+        } else {
+            const newLog = new LogModel({
+                userId,
+                serviceUsed,
+                url: data.url || null,
+                text: data.text || null,
+                label: data.label || null,
+                createdAt: new Date()
+            });
+            await newLog.save();
+        }
+
         console.log('Usage logged successfully');
     } catch (error) {
-        console.error('Failed to log usage:', error);
+        console.error('Failed to log usage:', error.message);
+        throw error; // إعادة رمي الخطأ للتعامل معه في المستوى الأعلى إذا لزم الأمر
     }
 }
 
+async function AdminlogUsage(userId, serviceUsed, data) {
+    await logUsageData(AdminUsageLog, userId, serviceUsed, data);
+}
 
-module.exports = { logUsage };
+async function logUsage(userId, serviceUsed, data) {
+    await logUsageData(UsageLog, userId, serviceUsed, data);
+}
+
+module.exports = { AdminlogUsage, logUsage };
